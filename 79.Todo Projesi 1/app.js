2a136b0c2a1a9f788b2df29d6b924852e8a5edec
Sezer5 <76732537+Sezer5@users.noreply.github.com>
2023-08-30 23:04:53 +0300

    first commit

---
+++ 79.Todo Projesi 1/app.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".list-group")[0];
+const secondCardBody = document.querySelectorAll(".list-group")[1];
+const clearButton = document.querySelectorAll("#clearButton");
+let todos = [];
+runEvents();
+
+function runEvents(){
+    form.addEventListener("submit",addTodo);
+}
+
+function addTodo(e){
+    const inputText = addInput.value.trim();
+    if(inputText==null || inputText==""){
+        alert("Lütfen Bir Değer Giriniz");
+    }else{
+        addTodoToUI(inputText);
+        addTodoToStorage(inputText);
+    }
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo){
+/*
+     <li class="list-group-item d-flex justify-content-between">Todo 1
+                            <a href="#" class="delete-item">
+                                <i class="fa fa-remove"></i>
+                            </a>
+                        </li>
+*/
+     const li = document.createElement("li");
+     li.className="list-group-item d-flex justify-content-between";
+     li.textContent=newTodo;
+     const a = document.createElement("a");
+     a.href="#";
+     a.className="delete-item";
+     const i = document.createElement("i");
+     i.className="fa fa-remove";
+     a.appendChild(i);
+     li.appendChild(a);
+     todoList.appendChild(li);
+     addInput.value="";
+
+}
+
+function addTodoToStorage(newTodo){
+    checkTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function checkTodosFromStorage(){
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+}
